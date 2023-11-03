eceaad91729aa20b9f3fa5ee788e2467ef8b2385
Michael Yogar <59573165+MichaelYogar@users.noreply.github.com>
2023-11-03 17:23:02 -0400

    Delete ui directory

--- ui/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <div className="bg-nice-colour flex justify-center sticky top-0">
-      <h1 className="text-6xl font-normal leading-normal mt-0 mb-2">Tetra</h1>
-    </div>
-  );
-};
-
-export default Header;
